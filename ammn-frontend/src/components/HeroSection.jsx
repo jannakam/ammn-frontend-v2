bf7795694552e2ded8aaf1a88bf405af6512fef3
jannakam <jannakalmuqaisib@gmail.com>
2024-11-25 18:22:28 +0300

    landing page

--- ammn-frontend/src/components/HeroSection.jsx
+++ ammn-frontend/src/components/HeroSection.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function HeroSection() {
-    <div class="banner">
-    <div class="slider" style="--quantity: 10">
-        <div class="item" style="--position: 1"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 2"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 3"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 4"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 5"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 6"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 7"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 8"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 9"><img src="card1.png" alt="" /></div>
-        <div class="item" style="--position: 10"><img src="card1.png" alt="" /></div>
-    </div>
-    <div class="content">
-        <h1 data-content="CSS ONLY">
-            CSS ONLY
-        </h1>
-        <div class="author">
-            <p>By Wahab, Janna, Saja and Nora</p>
+    
+    return (
+        <div className="banner h-screen">
+            <div className="slider" style={{ '--quantity': '8' }}>
+                {[...Array(8)].map((_, i) => (
+                    <div
+                        className="item"
+                        style={{ '--position': i + 1 }}
+                        key={i}
+                    >
+                        <Image
+                            src="/card2.png"
+                            alt={`Card ${i + 1}`}
+                            width={280}
+                            height={200}
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className="content">
+                
+                <div className="model"></div>
+            </div>
         </div>
-        <div class="model"></div>
-    </div>
-</div>
-
+    );
 }
